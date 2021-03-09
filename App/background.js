7a5f5279dc53b43c5f898d3c7a4a00e64eaea2de
vicky <srivicky2000@gmail.com>
2021-03-09 16:41:00 +0530

    CD - Download items prompt functionality done ok

--- App/background.js
+++ App/background.js
@@ -1,43 +1,71 @@
-chrome.tabs.onActivated.addListener(tab => {
-    console.log(tab)
-    chrome.tabs.get(tab.tabId, current_tab => {
-        console.log(current_tab.url)
-    })
-})
-chrome.tabs.executeScript(null, { file: './foreground.js' }, () => {
-    console.log('Injected')
+// chrome.tabs.onActivated.addListener(tab => {
+//         console.log(tab)
+//         chrome.tabs.get(tab.tabId, current_tab => {
+//             console.log(current_tab.url)
+//         })
+//     })
+// chrome.tabs.executeScript(null, { file: './foreground.js' }, () => {
+//     console.log('Injected')
+//     chrome.downloads.search({}, (all_d) => {
+//         all_d.map((D) => {
+//             // console.log(D.filename)
+//             // console.log(D.filename.slice(22))
+//             // console.log(D.finalUrl)
+//             console.log(D)
+//                 // console.log(D.finalUrl)
+
+
+//         })
+
+//     })
+// })
+
+
+// chrome.downloads.onDeterminingFilename.addListener(function(item, suggest) {
+//     console.log(item.finalUrl)
+//     chrome.downloads.search({}, (all_d) => {
+//         all_d.map((D) => {
+//             if (item.finalUrl == D.url) {
+//                 // suggest({
+//                 //     filename: item.filename,
+//                 //     conflict_action: 'prompt',
+//                 //     conflictAction: 'prompt',
+
+//                 // });
+//                 console.log('Already there')
+//             } else {
+//                 console.log('Not there')
+
+//             }
+//         })
+//     });
+
+// });
+
+
+chrome.downloads.onChanged.addListener(function(item, suggest) {
+    console.log(item.filename.current)
     chrome.downloads.search({}, (all_d) => {
-        all_d.map((D) => {
-            // console.log(D.filename)
-            // console.log(D.filename.slice(22))
-            // console.log(D.finalUrl)
-            console.log(D.url)
-                // console.log(D.finalUrl)
 
 
-        })
+        for (i = 0; i < all_d.length; i++) {
+            // console.log(all_d[i].filename)
+            if (item.filename.current === all_d[i].filename) {
+                console.log('file already exist')
+                chrome.downloads.onDeterminingFilename.addListener(function(item, suggest) {
+                    suggest({
+                        filename: item.filename,
+                        conflict_action: 'prompt',
+                        conflictAction: 'prompt',
 
-    })
-})
+                    });
 
-
-chrome.downloads.onDeterminingFilename.addListener(function(item, suggest) {
-    console.log(item.finalUrl)
-    chrome.downloads.search({}, (all_d) => {
-        all_d.map((D) => {
-            if (item.finalUrl == D.url) {
-                // suggest({
-                //     filename: item.filename,
-                //     conflict_action: 'prompt',
-                //     conflictAction: 'prompt',
-
-                // });
-                console.log('Already there')
-            } else {
-                console.log('Not there')
+                });
+                break
 
             }
-        })
+
+        }
     });
 
 });
