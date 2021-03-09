7a5d04b6bd603776b38c53c34acb53346876b0d1
vicky <srivicky2000@gmail.com>
2021-03-09 15:56:36 +0530

    CD - Download items prompt functionality

--- App/background.js
+++ App/background.js
@@ -8,10 +8,25 @@ chrome.tabs.executeScript(null, { file: './foreground.js' }, () => {
     console.log('Injected')
     chrome.downloads.search({}, (all_d) => {
         all_d.map((D) => {
-            console.log(D.filename)
-            console.log(D.filename)
-            console.log(D.finalUrl)
+            // console.log(D.filename)
+            // console.log(D.filename.slice(22))
+            // console.log(D.finalUrl)
+            console.log(D.url)
+                // console.log(D.finalUrl)
+
+
         })
 
     })
 })
+
+
+chrome.downloads.onDeterminingFilename.addListener(function(item, suggest) {
+    suggest({
+        filename: item.filename,
+        conflict_action: 'prompt',
+        conflictAction: 'prompt',
+
+    });
+
+});
