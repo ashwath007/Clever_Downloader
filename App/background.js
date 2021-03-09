b8945388fadba5b93c1f484c4044a84c73890fb6
vicky <srivicky2000@gmail.com>
2021-03-09 15:18:59 +0530

    CD - Download items got

--- App/background.js
+++ App/background.js
@@ -1,1 +1,17 @@
-console.log('From Background')
+chrome.tabs.onActivated.addListener(tab => {
+    console.log(tab)
+    chrome.tabs.get(tab.tabId, current_tab => {
+        console.log(current_tab.url)
+    })
+})
+chrome.tabs.executeScript(null, { file: './foreground.js' }, () => {
+    console.log('Injected')
+    chrome.downloads.search({}, (all_d) => {
+        all_d.map((D) => {
+            console.log(D.filename)
+            console.log(D.filename)
+            console.log(D.finalUrl)
+        })
+
+    })
+})
